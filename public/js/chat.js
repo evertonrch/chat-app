412a04301dee11eac7d88b91c6226a8cedc687ea
Everton Rocha <imevertonrch@gmail.com>
2024-12-18 03:15:03 -0300

    implementando eventos e renderizando templates com Mustache

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,53 @@
-io()
+const server = io()
+
+// elements
+const $messageForm = document.querySelector(".form")
+const $messageInput = $messageForm.querySelector("input")
+const $buttonLocation = document.querySelector("#send-location")
+const $locationLink = document.querySelector(".link")
+const $messages = document.querySelector("#messages")
+const $position = document.querySelector("#position")
+
+
+// templates
+const messageTemplate = document.querySelector("#message-template").innerHTML
+const positionTemplate = document.querySelector("#position-template").innerHTML
+
+server.on("message", (message) => {
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+})
+
+$messageForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const message = $messageInput.value
+
+    server.emit("sendMessage", message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log("delivered")
+    })
+})
+
+$buttonLocation.addEventListener("click", () => {
+    $buttonLocation.setAttribute("disabled", "disabled")
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        server.emit("position", { latitude, longitude }, (message) => {
+
+            $buttonLocation.removeAttribute("disabled")
+        })
+    })
+})
+
+server.on("linkPosition", ({ latitude, longitude }) => {
+    const url = `https://google.com/maps?q=${latitude},${longitude}`
+    const html = Mustache.render(positionTemplate, { url })
+    $position.innerHTML = ""
+    $position.insertAdjacentHTML("beforeend", html)
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path")
 const express = require("express")
 const http = require("http")
 const socketio = require("socket.io")
+const Filter = require("bad-words")
 
 const app = express()
 const server = http.createServer(app)
@@ -11,8 +12,32 @@ const PORT = process.env.PORT | 3000
 const publicDirPath = path.join(__dirname, "../public")
 app.use(express.static(publicDirPath))
 
-io.on("connection", () => {
+
+io.on("connection", (socket) => {
     console.log("new websocket connection")
+
+    socket.emit("message", "Welcome")
+
+    socket.broadcast.emit("message", "A new user joined")
+
+    socket.on("sendMessage", (message, callback) => {
+        const filter = new Filter()
+        if(filter.isProfane(message)) {
+            return callback("profanity is not allowed")
+        }
+
+        io.emit("message", message)
+        callback()
+    })
+
+    socket.on("position", (pos, callback) => {
+        io.emit("linkPosition", pos)
+        callback()
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left")
+    })
 })
 
 server.listen(PORT)
